1a7127fd9e7186500f333cb171a9037226a59464
Eleazer85 <audwineleazer@gmail.com>
2024-01-03 11:03:14 +0700

    Changing the loop to edit all_word (Problem encounter)

--- training.py
+++ training.py
@@ -10,21 +10,19 @@ all_words = []
 tags = []
 xy = []
 Stemmer = nltk.stem.PorterStemmer()
+
+# Define a regular expression pattern for punctuation
+punctuation_pattern = ["?","!",",","."]
+
 # loop through each sentence in our intents patterns
 for intent in intents['intents']: 
     tag = intent['tag']
     tags.append(tag)
     for pattern in intent['patterns']:
         w = Tokenizer(pattern)
+        w = [Stemmer.stem(w) for w in all_words if w not in punctuation_pattern]
         all_words.extend(w)
         xy.append((w,tag))
      
-# Define a regular expression pattern for punctuation
-punctuation_pattern = ["?","!",",","."]
-     
 # Filter out non-string elements
-print(f"{xy} \n\n\n")
-print(f"{tag} \n\n\n")
-print(f"{all_words} \n\n\n")
-all_words = [Stemmer.stem(w) for w in all_words if w not in punctuation_pattern]
 print(all_words)
